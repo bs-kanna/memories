c866a8ae941bcf1f4e05586d937856bd11984b55
bs-kanna <168420768+bs-kanna@users.noreply.github.com>
2024-10-23 22:14:07 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'https://memories-server-72sf.onrender.com';
+const url = 'https://memories-server-72sf.onrender.com/posts';
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
