cd00f278fcd72fa4015a2d8353dbfdd4e0df66a3
bs-kanna <168420768+bs-kanna@users.noreply.github.com>
2024-11-03 21:29:53 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use('/posts', postRoutes);
 
 // Database Connection
-const CONNECTION_URL = 'mongodb+srv://mongo:1234@fullstack.duvvdgy.mongodb.net/?retryWrites=true&w=majority&appName=fullstack';
+const CONNECTION_URL = 'mongodb+srv://mongo:0000@fullstack.duvvdgy.mongodb.net/?retryWrites=true&w=majority&appName=fullstack';
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
